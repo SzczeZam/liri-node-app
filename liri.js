b77d985689df734e0e399eef9144cb4fe70877ed
Max Szczepaniak <wookslandline@protonmail.com>
2019-07-28 22:46:50 -0400

    fixed recursive bug. Formatted text output

--- liri.js
+++ liri.js
@@ -5,47 +5,45 @@ var keys = require("./keys.js");
 var Spotify = require("node-spotify-api")
 var spotify = new Spotify(keys.spotify);
 
-
-var inputCmd = process.argv[2]
-var inputValue = process.argv[3]
-
-function brains(){
-
-
+function brains(inputCmd, inputValue){
     switch (inputCmd){
         case 'concert-this':
-            concertThis()
+            concertThis(inputValue)
             break
             
         case 'spotify-this-song':
-            spotifyThisSong()
+            spotifyThisSong(inputValue)
             break
 
         case 'movie-this':
-            movieThis()
+            movieThis(inputValue)
             break
             
         case 'do-what-it-says':
-            doWhatItSays()
+            doWhatItSays(inputValue)
             break
     }
 
 }
 
 
-function concertThis() {
-    console.log('grabbing concert data')
-    var bitURL = `https://rest.bandsintown.com/artists/${inputValue}/events?app_id=codingbootcamp`
+function concertThis(band) {
+    console.log(`grabbing concert data
+    `)
+    var bitURL = `https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`
     axios.get(bitURL)
         .then(function(response){
             var data = response.data
             for (events of data){
                 var event = {
-                    name : events.venue.name,
-                    location : `${events.venue.city}, ${events.venue.region}, ${events.venue.country}`,
-                    date : events.datetime
+                    name : `Venue Name: 
+                        ${events.venue.name}`,
+                    location : `Location: 
+                        ${events.venue.city}, ${events.venue.region}, ${events.venue.country}`,
+                    date : `Datetime: 
+                        ${events.datetime}`,
                 }
-                console.table([event])
+                print(event)
             }
         })
         .catch(function(err){
@@ -54,46 +52,61 @@ function concertThis() {
 
 }
 function spotifyThisSong() {
-    console.log('pulling data from spotify')
+    console.log(`pulling data from spotify...
+    `)
     var search = inputValue
     if (search === undefined) { search = 'Ace of Base The Sign'}
     spotify
         .search({type: 'track', query: search, limit: 2})
         .then(function(response){
             var result = {
-                song : response.tracks.items[0].name,
-                artist : response.tracks.items[0].artists[0].name,
-                link : response.tracks.items[0].preview_url,
-                album: response.tracks.items[0].album.name,
+                song : `Song Title: 
+                    ${response.tracks.items[0].name}`,
+                artist : `Artist/s: 
+                    ${response.tracks.items[0].artists[0].name}`,
+                link : `Preview Link: 
+                    ${response.tracks.items[0].preview_url}`,
+                album: ` Album: 
+                    ${response.tracks.items[0].album.name}`,
             }
-            console.table(result)
+            print(result)
         })
         .catch(function(error){
             console.log(error)
         })
     
 }
-function movieThis() {
-    console.log('combing through IMDB')
-    var query = inputValue
+function movieThis(film) {
+    console.log(`Combing through the database...
+    `)
+
+    var query = film
     if (query === undefined) { query = 'Mr.Nobody'}
     
     var movieURL = `http://www.omdbapi.com/?apikey=trilogy&t=${query}`
     axios.get(movieURL)
         .then(function(response){
-            console.log(response.data)
             var movie = {
-                title: response.data.Title,
-                year: response.data.Year,
-                IMDBrating: response.data.Ratings[0].Value,
-                RTrating: response.data.Ratings[1].Value,
-                country: response.data.Country,
-                language: response.data.Language,
-                actors: response.data.Actors,
-                plot: response.data.Plot,
+                title: `Title: 
+                    ${response.data.Title}`,
+                year: `Year Released: 
+                    ${response.data.Year}`,
+                IMDBrating: `IMDB Rating: 
+                    ${response.data.Ratings[0].Value}`,
+                RTrating: `Rotten Tomatoes: 
+                    ${response.data.Ratings[1].Value}`,
+                country: `Publishing Country: 
+                    ${response.data.Country}`,
+                language: `Language: 
+                    ${response.data.Language}`,
+                actors: `Actors: 
+                    ${response.data.Actors}`,
+                plot: `Plot Summary: 
+                    ${response.data.Plot}`,
             }
-            console.log(movie)
             
+            print(movie)
+
         })
         .catch(function(err){
             console.log(err)
@@ -101,17 +114,32 @@ function movieThis() {
 }
 function doWhatItSays() {
     console.log('Doing what it says...')
-    fs.read('./random.txt', 'utf8', function(error, data){
+    fs.readFile('./random.txt', 'utf8', function(error, data){
         if(error){console.log(error)}
 
         data = data.split(',')
         console.log(data)
 
 
-        inputCmd = data[0]
-        inputValue = data[1]
+        newCmd = data[0]
+        newValue = data[1]
+        console.log
+        brains(newCmd,newValue)
     })
-    brains()
 }
 
-brains()
+function print(object){
+    console.log(`
+    `)
+    for (val in object){
+        console.log(object[val])      
+    }
+    console.log(`
+    `)
+}
+
+
+
+var cmd = process.argv[2]
+var value = process.argv[3]
+brains(cmd,value)
